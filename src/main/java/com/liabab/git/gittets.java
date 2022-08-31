e4509b6adfeaa7455b6fff32ae3bffe58d845852
Libaba <Libaba@qq.com>
2022-08-31 17:34:00 +0800

    Idea third

--- src/main/java/com/liabab/git/gittets.java
+++ src/main/java/com/liabab/git/gittets.java
@@ -4,5 +4,6 @@ public class gittets {
     public static void main(String[] args) {
         System.out.println("hello git!");
         System.out.println("hello git222!");
+        System.out.println("hello git333!");
     }
 }
